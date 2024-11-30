53fbe4e415410d029a70f7c416b4d0458f158b41
Zefira Shannon <earthlingzephyr@gmail.com>
2024-11-30 03:34:48 -0800

    Reformat with new js mode settings

--- site.js
+++ site.js
@@ -1,22 +1,22 @@
-var Metalsmith  = require('metalsmith');
-var markdown    = require('@metalsmith/markdown');
-var layouts     = require('@metalsmith/layouts');
+var Metalsmith = require('metalsmith');
+var markdown = require('@metalsmith/markdown');
+var layouts = require('@metalsmith/layouts');
 
 Metalsmith(__dirname)
-  .metadata({
-    sitename: "Genital Autonomy Collective",
-    siteurl: "https://genital-autonomy-collective.github.io",
-    generatorname: "Metalsmith",
-    generatorurl: "https://metalsmith.io/"
-  })
-  .source('./source')
-  .destination('./build')
-  .clean(false)
-  .use(markdown())
-  .use(layouts({
-    "default": "main.hbs",
-    "pattern": "**/*.html"
-  }))
-  .build(function(err, files) {
-    if (err) { throw err; }
-  });
+    .metadata({
+        sitename: "Genital Autonomy Collective",
+        siteurl: "https://genital-autonomy-collective.github.io",
+        generatorname: "Metalsmith",
+        generatorurl: "https://metalsmith.io/"
+    })
+    .source('./source')
+    .destination('./build')
+    .clean(false)
+    .use(markdown())
+    .use(layouts({
+        "default": "main.hbs",
+        "pattern": "**/*.html"
+    }))
+    .build(function(err, files) {
+        if (err) { throw err; }
+    });
